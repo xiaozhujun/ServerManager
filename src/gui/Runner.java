a8edc0e3076a995080945de591c763532d3395ab
xiaozhujun <xiaozhujun520@163.com>
2013-08-21 14:27:28 +0800

    微改

--- src/gui/Runner.java
+++ src/gui/Runner.java
@@ -11,6 +11,7 @@ public class Runner {
         jf.add(sm.$$$getRootComponent$$$());
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         jf.setSize(700,400);
+        jf.setResizable(false);
         jf.setLocation((screenSize.width - jf.getWidth()) / 2, (screenSize.height - jf.getHeight()) / 2);
         jf.setTitle("港机数据采集服务配置");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
